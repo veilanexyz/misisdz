aae0488fb60b193ed3be5950e5b637db12600c44
veilanexyz <114360205+veilanexyz@users.noreply.github.com>
2022-10-07 11:23:03 +0300

    Update 0004a.cpp

--- 0004a.cpp
+++ 0004a.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-using namespace std;
 int main() {
     int w = 0;
-    cin >> w;
+    std::cin >> w;
     if (w % 2 == 0 && w > 2)
-        cout<<"YES";
+        std::cout<<"YES";
      
     else
-        cout<<"NO";
+        std::cout<<"NO";
     
 }
