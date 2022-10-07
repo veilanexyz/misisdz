c12bc5a2e2b88c3892231ca1d43e8a686ba0bf3d
veilanexyz <114360205+veilanexyz@users.noreply.github.com>
2022-10-07 11:23:38 +0300

    Update 0217a.cpp

--- 0217a.cpp
+++ 0217a.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-using namespace std;
 int main() {
     int x = 0, a, b, c, d;
-    cin >> x;
+    std::cin >> x;
     for (int i = x + 1; i <= 10000; i++){
         a = i / 1000;
         b = (i / 100) % 10;
         c = (i % 100) / 10;
         d = i % 10;
         if (a != b && b != c && c != d && a != c && a != d && b != d){
-            cout << i;
+            std::cout << i;
             break;
         }
     }
