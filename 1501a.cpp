6d37c117ed882ce893b6dd2ae6353134fdefb143
veilanexyz <114360205+veilanexyz@users.noreply.github.com>
2022-10-11 10:11:08 +0300

    Update 1501a.cpp

--- 1501a.cpp
+++ 1501a.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 int main()
 {
     int t;
     std::cin >> t;
     for (int i = 0; i < t; i++) {
-        
-        int n, k;
+
+        int n, k, d;
         std::cin >> n;
-        int* a = new int [n * 2];
-        int* b = new int[n];
+        std::vector<int> a;
+        std::vector<int> b;
         for (int j = 0; j < n * 2; j++) {
-            std::cin >> a[j];
+            std::cin >> d;
+            a.push_back(d);
         }
         for (int j = 0; j < n; j++) {
-            std::cin >> b[j];
+            std::cin >> d;
+            b.push_back(d);
         }
-        for (int j = 0; j < n*2; j+=2) {
+        for (int j = 0; j < n * 2; j += 2) {
             k = a[j];
-            a[j] = a[j] + b[j];
-            a[j + 1] = a[j] + ceil((a[j + 1] - k) / 2);
+            a[j] = a[j] + b[j / 2];
+            a[j + 1] = a[j] + ceil((a[j + 1] - a[j]) / 2);
         }
-        std::cout << a[n*2-1];
+        std::cout << a[n * 2 - 1]<<std::endl;
     }
     return 0;
 }
