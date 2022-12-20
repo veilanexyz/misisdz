4f9e3aa09ba89f349919bb433c362a9710ca133d
veilanexyz <114360205+veilanexyz@users.noreply.github.com>
2022-12-20 10:21:57 +0300

    Create bpm_22_3_2.cpp

---
+++ bpm_22_3_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main() {
+	int k = 0, n = 0;
+	double x, y;
+	std::cin >> n;
+	for (int i = 0; i < n; i++) {
+		std::cin >> x >> y;
+		if (x <= 2 && x >= -2) {
+			if (y <= 1 && y >= -1) {
+				
+			}
+		}
+	}
+}
